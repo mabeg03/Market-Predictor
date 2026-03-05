bfb20209e4727b576cc683c22b8df175116c088c
mabeg03 <themunnu3@gmail.com>
2026-03-05 10:15:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 4000;
